4c0fb6a178f0a50b645fb986ab5bf573f3f7f7db
Giovani Machado <giovani.machado@gmail.com>
2020-04-09 21:53:53 -0300

    type: feat
    Prepare files to deploy in Heroku

--- app.js
+++ app.js
@@ -191,6 +191,6 @@ app.post("/:pageName", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server running on 3000");
 });
